bfbd1194cce9a5aeabc0e7e0f1fd97aaef79c189
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-10-04 20:04:07 -0500

    comiting java files and layouts

---
+++ app/src/main/java/com/example/parkingmanager/BookSlot.java
@@ -0,0 +1,14 @@
+package com.example.parkingmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookSlot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_slot);
+    }
+}
