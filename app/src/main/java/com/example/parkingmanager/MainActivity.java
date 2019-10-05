2db80b7ece2bbf97b2414b9d68d972bd393481f5
Lahari <lahari12397@gmail.com>
2019-10-04 23:22:01 -0500

    intent created for button search for availability commited

--- app/src/main/java/com/example/parkingmanager/MainActivity.java
+++ app/src/main/java/com/example/parkingmanager/MainActivity.java
@@ -24,4 +24,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(MainActivity.this, SignupPage.class));
         this.finish();
     }
+
 }
--- app/src/main/java/com/example/parkingmanager/VehicleInformation.java
+++ app/src/main/java/com/example/parkingmanager/VehicleInformation.java
@@ -2,7 +2,9 @@ package com.example.parkingmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class VehicleInformation extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class VehicleInformation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_information); 
+    }
+
+    public void bookSlot(View v){
+        startActivity(new Intent(VehicleInformation.this, BookSlot.class));
+        this.finish();
     }
 }
