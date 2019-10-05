229d5f1a00a9364fd5a3d8f0c8a9c972d00c2bbf
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-10-04 22:52:03 -0500

    crated intent for signup page and passed it to startActivity method

--- app/src/main/java/com/example/parkingmanager/MainActivity.java
+++ app/src/main/java/com/example/parkingmanager/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signUp(View v){
-        
+        startActivity(new Intent(MainActivity.this, SignupPage.class));
+        this.finish();
     }
 }
