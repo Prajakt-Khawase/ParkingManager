c79319f5fbc8bf026bea358d689ff261bb35c007
nikitha <nikitha0618@gmail.com>
2019-10-04 23:48:54 -0500

    signup method

--- app/src/main/java/com/example/parkingmanager/SignupPage.java
+++ app/src/main/java/com/example/parkingmanager/SignupPage.java
@@ -2,7 +2,9 @@ package com.example.parkingmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupPage extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SignupPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page); 
+    }
+    public void signUp(View v){
+        startActivity(new Intent(SignupPage.this, MainActivity.class));
+
     }
 }
