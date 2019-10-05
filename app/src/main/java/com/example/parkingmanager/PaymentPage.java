b8a88f2e552dbb9780648784fb2b8c6ded1bd149
Lahari <lahari12397@gmail.com>
2019-10-04 23:43:06 -0500

    Payment page textviews added

--- app/src/main/java/com/example/parkingmanager/PaymentPage.java
+++ app/src/main/java/com/example/parkingmanager/PaymentPage.java
@@ -2,7 +2,9 @@ package com.example.parkingmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PaymentPage extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class PaymentPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_page); 
     }
+    public void invokeLogin(View v){
+        startActivity(new Intent(PaymentPage.this, CustomerLogout.class));
+
+
+    }
+
+
 }
