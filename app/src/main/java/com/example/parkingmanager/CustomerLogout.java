9634b724911e4da8429f9929b2ee2d18e7d146c0
Lahari <lahari12397@gmail.com>
2019-10-06 17:38:51 -0500

    logout button onclick method commited

--- app/src/main/java/com/example/parkingmanager/CustomerLogout.java
+++ app/src/main/java/com/example/parkingmanager/CustomerLogout.java
@@ -2,13 +2,22 @@ package com.example.parkingmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CustomerLogout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_logout); 
+        setContentView(R.layout.activity_customer_logout);
     }
+        public void onlogout(View v){
+            startActivity(new Intent(CustomerLogout.this, MainActivity.class));
+            this.finish();
+
+        }
+
+
 }
