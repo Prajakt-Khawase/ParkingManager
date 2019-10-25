6aaf9035ec1eabecc1c29d70e60f2a413259d99f
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-10-25 17:59:37 -0500

    Added new Attribute Mobile

--- app/src/main/java/com/example/parkingmanager/SQLiteHelper.java
+++ app/src/main/java/com/example/parkingmanager/SQLiteHelper.java
@@ -12,6 +12,7 @@ public class SQLiteHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME_MANAGER = "MANAGER";
     public static final String COLUMN_ID = "ID";
     public static final String COLUMN_EMAIL = "EMAIL";
+    public static final String COLUMN_MOBILE = "MOBILE";
 
     public SQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
