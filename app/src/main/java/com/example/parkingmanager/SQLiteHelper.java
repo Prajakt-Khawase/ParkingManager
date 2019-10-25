af7bb904110ad135fc4571a392f7fafea3ace598
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-10-25 17:44:29 -0500

    Moved the class to parking Manager package

---
+++ app/src/main/java/com/example/parkingmanager/SQLiteHelper.java
@@ -0,0 +1,25 @@
+package com.example.parkingmanager;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "SQLiteDatabase.db";
+
+    public SQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        // TODO Auto-generated constructor stub
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
