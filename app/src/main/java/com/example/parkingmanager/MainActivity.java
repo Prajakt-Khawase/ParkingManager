165b97473faa3daedeb36a8a7564d1ec044cb588
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-10-25 17:12:02 -0500

    Created A button For Login.

--- app/src/main/java/com/example/parkingmanager/MainActivity.java
+++ app/src/main/java/com/example/parkingmanager/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button login;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
