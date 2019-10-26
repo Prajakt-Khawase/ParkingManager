03877c3d276c2ad02cf64aee4ca9da6f9787d4fa
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-10-25 19:49:09 -0500

    Declared and Initialize constants.

--- app/src/main/java/com/example/parkingmanager/SharedPrefrenceUtilities.java
+++ app/src/main/java/com/example/parkingmanager/SharedPrefrenceUtilities.java
@@ -1,4 +1,11 @@
 package com.example.parkingmanager;
 
 public class SharedPrefrenceUtilities {
+
+    //SharedPreference Key
+    public static final String spIsLoggedin = "isLoggedin";
+    public static final String spFirstName ="name";
+    public static final String spEmail = "email";
+    public static final String spUserId = "userId";
+    public static final String spMobileNo = "mobileNo";
 }
