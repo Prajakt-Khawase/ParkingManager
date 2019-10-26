cb516969810575302ca437ff7662e5741981e995
Lahari <lahari12397@gmail.com>
2019-10-25 19:37:07 -0500

    Inflated the layout to convert view of xml file gridview slot

--- app/src/main/java/com/example/parkingmanager/FourWheelerAdapter.java
+++ app/src/main/java/com/example/parkingmanager/FourWheelerAdapter.java
@@ -1,9 +1,14 @@
 package com.example.parkingmanager;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -32,8 +37,44 @@ public class FourWheelerAdapter extends BaseAdapter {
         return 0;
     }
 
+    public static class ViewHolder
+    {
+        public ImageView imgViewFlag;
+        public TextView slottext;
+        public RelativeLayout slotLayout;
+    }
+
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder view;
+
+        if(convertView==null)
+        {
+
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.gridview_slot, null, false);
+            view = new ViewHolder();
+
+            view.slottext = convertView.findViewById(R.id.slot);
+            view.slotLayout = convertView.findViewById(R.id.slotLayout);
+
+            view.slottext.setText("SLOT\n"+(position+1));
+
+            view.slotLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Toast.makeText(context, "Book Car - "+slotList.get(position), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            convertView.setTag(view);
+        }
+        else
+        {
+            view = (ViewHolder) convertView.getTag();
+        }
+
+        return convertView;
     }
 }
