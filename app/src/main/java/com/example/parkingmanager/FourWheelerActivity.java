442b4241fe084d4f5a8b8018fe5f41e9e4b86161
Lahari <lahari12397@gmail.com>
2019-10-25 20:27:10 -0500

    Added slots using Array list and connected the layout file to four wheeler activity

--- app/src/main/java/com/example/parkingmanager/FourWheelerActivity.java
+++ app/src/main/java/com/example/parkingmanager/FourWheelerActivity.java
@@ -18,5 +18,37 @@ public class FourWheelerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_four_wheeler);
+        slotList=new ArrayList<>();
+        slotList.add("SLOT 1");
+        slotList.add("SLOT 2");
+        slotList.add("SLOT 3");
+        slotList.add("SLOT 4");
+        slotList.add("SLOT 5");
+        slotList.add("SLOT 6");
+        slotList.add("SLOT 7");
+        slotList.add("SLOT 8");
+        slotList.add("SLOT 9");
+        slotList.add("SLOT 10");
+        slotList.add("SLOT 11");
+        slotList.add("SLOT 12");
+        slotList.add("SLOT 13");
+        slotList.add("SLOT 14");
+        slotList.add("SLOT 15");
+        slotList.add("SLOT 16");
+        slotList.add("SLOT 17");
+        slotList.add("SLOT 18");
+        slotList.add("SLOT 19");
+        slotList.add("SLOT 20");
+        slotList.add("SLOT 21");
+        slotList.add("SLOT 22");
+        slotList.add("SLOT 23");
+        slotList.add("SLOT 24");
+        slotList.add("SLOT 25");
+
+        mAdapter = new FourWheelerAdapter(context, slotList);
+        gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(mAdapter);
+
     }
 }
