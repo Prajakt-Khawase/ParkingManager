43fe897c738915a103d63e2923e791cfcf4f082f
manasagoriparthi <manasaprasad97@gmail.com>
2019-10-25 20:01:55 -0500

    twowheeleradapter main method @manasa

---
+++ app/src/main/java/com/example/parkingmanager/TwoWheelerAdapter.java
@@ -0,0 +1,43 @@
+package com.example.parkingmanager;
+
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class TwoWheelerAdapter extends BaseAdapter {
+    private ArrayList<String> slotList;
+
+    Context context;
+
+    public TwoWheelerAdapter(Context context, ArrayList<String> slotList) {
+        super();
+        this.slotList = slotList;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+}
+
