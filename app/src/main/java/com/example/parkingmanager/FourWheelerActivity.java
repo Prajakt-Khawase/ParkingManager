e6f8da852e87e521e545df1d213f70c23b2c3bfa
Lahari <lahari12397@gmail.com>
2019-10-25 20:06:41 -0500

    implemented method for four wheeler activity

--- app/src/main/java/com/example/parkingmanager/FourWheelerActivity.java
+++ app/src/main/java/com/example/parkingmanager/FourWheelerActivity.java
@@ -1,4 +1,22 @@
 package com.example.parkingmanager;
 
-public class FourWheelerActivity {
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.GridView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class FourWheelerActivity extends AppCompatActivity {
+    private FourWheelerAdapter mAdapter;
+    private ArrayList<String> slotList;
+    private GridView gridView;
+    Context context=FourWheelerActivity.this;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
