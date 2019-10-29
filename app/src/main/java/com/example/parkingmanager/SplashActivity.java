e5ed740eb1f947454de8c71ac5b8bf6c4bc13302
mushafiq <43075639+mushafiq@users.noreply.github.com>
2019-10-29 10:07:20 -0500

    Creating a new Thread

--- app/src/main/java/com/example/parkingmanager/SplashActivity.java
+++ app/src/main/java/com/example/parkingmanager/SplashActivity.java
@@ -11,12 +11,13 @@ import android.view.WindowManager;
 public class SplashActivity extends AppCompatActivity {
 
     private boolean isLoggedIn = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        isLoggedIn = SharedPrefrenceUtilities.getSPbooleanValue(SplashActivity.this,SharedPrefrenceUtilities.spIsLoggedin);
+        isLoggedIn = SharedPrefrenceUtilities.getSPbooleanValue(SplashActivity.this, SharedPrefrenceUtilities.spIsLoggedin);
         Window window = getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -24,5 +25,11 @@ public class SplashActivity extends AppCompatActivity {
             getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark)); //status bar or the time bar at the top
         }
 
+        Thread thread = new Thread() {
+
+        };
+
+
     }
+
 }
