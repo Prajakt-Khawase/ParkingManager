717a7bd7dd02ff4c7ddfb2ae4612e731617c4d9a
mushafiq <43075639+mushafiq@users.noreply.github.com>
2019-10-29 10:04:52 -0500

    Checking the build version with lollipop and setting status bar or the time bar at the top

--- app/src/main/java/com/example/parkingmanager/SplashActivity.java
+++ app/src/main/java/com/example/parkingmanager/SplashActivity.java
@@ -1,7 +1,9 @@
 package com.example.parkingmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,6 +20,9 @@ public class SplashActivity extends AppCompatActivity {
         Window window = getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark)); //status bar or the time bar at the top
+        }
 
     }
 }
