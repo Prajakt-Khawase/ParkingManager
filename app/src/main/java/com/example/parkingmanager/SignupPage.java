597b153b3bb540db1067140fae23ea0fd1a94fa8
nikitha <nikitha0618@gmail.com>
2019-11-05 13:00:34 -0600

    Defined String attributes for fields

--- app/src/main/java/com/example/parkingmanager/SignupPage.java
+++ app/src/main/java/com/example/parkingmanager/SignupPage.java
@@ -9,7 +9,7 @@ import android.widget.EditText;
 
 public class SignupPage extends AppCompatActivity {
     EditText fnameED,lnameED,emailED,mobileED,passwordED,cnfrmPasswordED;
-    
+    String fname,lname,email,mobile,password,cnfrmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
