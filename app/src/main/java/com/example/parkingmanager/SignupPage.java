1d63fb9f7261e5e4e90271e7e96f0657885f679d
nikitha <nikitha0618@gmail.com>
2019-11-05 12:28:33 -0600

    Added an image view and created text_field.xml

--- app/src/main/java/com/example/parkingmanager/SignupPage.java
+++ app/src/main/java/com/example/parkingmanager/SignupPage.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SignupPage extends AppCompatActivity {
-
+    EditText fnameED,lnameED,emailED,mobileED,passwordED,cnfrmPasswordED;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
