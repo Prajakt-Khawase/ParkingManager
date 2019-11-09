f27df9d2598805f9d3e82b0889fbe02c628b267b
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-11-08 19:38:02 -0600

    Created Variables For button and ImageView

--- app/src/main/java/com/example/parkingmanager/HomeActivity.java
+++ app/src/main/java/com/example/parkingmanager/HomeActivity.java
@@ -3,12 +3,15 @@ package com.example.parkingmanager;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
     Context context=HomeActivity.this;
-
+    Button button1,button2,button3,button4;
+    ImageView logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
