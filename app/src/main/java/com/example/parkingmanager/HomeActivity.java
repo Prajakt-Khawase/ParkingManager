ec56b75d3ec3f37714709384a7892ae04194d960
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-11-08 18:56:19 -0600

    Created the Context Object.

--- app/src/main/java/com/example/parkingmanager/HomeActivity.java
+++ app/src/main/java/com/example/parkingmanager/HomeActivity.java
@@ -1,19 +1,22 @@
 package com.example.parkingmanager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    Context context=HomeActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
-    public void fourwheeler(View v){
 
+    @Override
+    public void onClick(View v) {
+    }
     }
 
-}
