abfe436db1f21ed70652cbaa9fc06097a2502698
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-11-08 18:38:28 -0600

    set the Listner if the user pressed the YES button.

--- app/src/main/java/com/example/parkingmanager/MainActivity.java
+++ app/src/main/java/com/example/parkingmanager/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.parkingmanager;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -38,32 +39,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void initilize()
-    {
+    private void initilize() {
         login = findViewById(R.id.login_button);
-        usernameED=findViewById(R.id.username);
-        passwordED=findViewById(R.id.password);
+        usernameED = findViewById(R.id.username);
+        passwordED = findViewById(R.id.password);
     }
-    private void validation()
-    {
 
-        username=usernameED.getText().toString().toLowerCase();
-        password=passwordED.getText().toString().toLowerCase();
-        if(username.isEmpty())
-        {
+    private void validation() {
+
+        username = usernameED.getText().toString().toLowerCase();
+        password = passwordED.getText().toString().toLowerCase();
+        if (username.isEmpty()) {
             usernameED.setError("Enter username");
-        }else if(!isValidEmail(username))
-        {
+        } else if (!isValidEmail(username)) {
             usernameED.setError("Enter valid username");
 
-        }else if(password.isEmpty())
-        {
+        } else if (password.isEmpty()) {
             passwordED.setError("Enter password");
 
-        }else {
+        } else {
 
             Cursor cursor = mSqLiteHelper.managerLogin(username, password);
-            int row =  cursor.getCount();
+            int row = cursor.getCount();
             if (cursor != null && cursor.getCount() != 0) {
                 cursor.moveToFirst();
                 for (int i = 1; i <= row; i++) {
@@ -74,20 +71,27 @@ public class MainActivity extends AppCompatActivity {
                     String s4 = cursor.getString(4);//password
                     // cursor.moveToNext();
                     //Toast.makeText(MainActivity.this, row2+" "+s0 + "ID : " + s1 + " " + s2 + " " + s3 + " " + s4, Toast.LENGTH_SHORT).show();
-                    SharedPrefrenceUtilities.setSPboolean(MainActivity.this,SharedPrefrenceUtilities.spIsLoggedin,true);
-                    Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                    SharedPrefrenceUtilities.setSPboolean(MainActivity.this, SharedPrefrenceUtilities.spIsLoggedin, true);
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                     startActivity(intent);
                     finish();
                 }
-            }else
-            {
+            } else {
                 Toast.makeText(MainActivity.this, "Invalid username password", Toast.LENGTH_SHORT).show();
             }
 
         }
-    } public void appCloseAlert(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("Close App?");
     }
 
+    public void appCloseAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Close App?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //handle yes logic here
+                finish();
+            }
+        });
+
+    }
 }
