9d2e94f4bc572ce207d93a9516e356d465dd4beb
Lahari <lahari12397@gmail.com>
2019-11-09 18:01:05 -0600

    Added intent for signup

--- app/src/main/java/com/example/parkingmanager/MainActivity.java
+++ app/src/main/java/com/example/parkingmanager/MainActivity.java
@@ -35,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        findViewById(R.id.loginSignup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignupPage.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 
