118019ced89c5b2ff745357b1473ca7fff8351e0
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-11-15 08:20:26 -0600

    Created a Handler and managed delay.

--- app/src/main/java/com/example/parkingmanager/HomeActivity.java
+++ app/src/main/java/com/example/parkingmanager/HomeActivity.java
@@ -70,7 +70,13 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         this.doubleBackToExitPressedOnce = true;
         Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
 
+        new Handler().postDelayed(new Runnable() {
 
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
         @Override
     public void onClick(View v) {
