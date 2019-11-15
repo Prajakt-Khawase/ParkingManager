485ab853f4b4310fdf3ef204e5984011de4cefa4
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-11-15 15:18:22 -0600

    Handled the backpress action.

--- app/src/main/java/com/example/parkingmanager/HomeActivity.java
+++ app/src/main/java/com/example/parkingmanager/HomeActivity.java
@@ -77,12 +77,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             }
     }
 
-        @Override
-    public void onBackPressed() {
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed();
-            return;
-        }
+
 
         this.doubleBackToExitPressedOnce = true;
         Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
@@ -162,6 +157,23 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         builder.show();
     }
 
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
 
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
 
 }
