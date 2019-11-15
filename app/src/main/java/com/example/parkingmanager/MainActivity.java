4a8e025558ea9b8b84e7a077d0245a3cd4610d9d
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-11-15 01:24:21 -0600

    created a new handler for postdelay.

--- app/src/main/java/com/example/parkingmanager/MainActivity.java
+++ app/src/main/java/com/example/parkingmanager/MainActivity.java
@@ -118,6 +118,12 @@ public class MainActivity extends AppCompatActivity {
     }
         this.doubleBackToExitPressedOnce = true;
         Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+        new Handler().postDelayed(new Runnable() {
 
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
