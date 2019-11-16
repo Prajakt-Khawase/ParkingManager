c3acce71a3858bec845d289671c0dbc5114a50dc
manasagoriparthi <manasaprasad97@gmail.com>
2019-11-15 21:41:30 -0600

    intent i  two wheeler activity @ s534782, manasa

--- app/src/main/java/com/example/parkingmanager/TwoWheelerActivity.java
+++ app/src/main/java/com/example/parkingmanager/TwoWheelerActivity.java
@@ -2,6 +2,7 @@ package com.example.parkingmanager;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.GridView;
 
