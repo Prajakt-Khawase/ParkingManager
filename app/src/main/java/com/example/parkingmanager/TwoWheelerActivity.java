fc4534863601d4927fad6408c167de18fd2cfdcf
manasagoriparthi <manasaprasad97@gmail.com>
2019-11-15 21:48:47 -0600

    simple date committer in two wheeler activity @ s534782, manasa

--- app/src/main/java/com/example/parkingmanager/TwoWheelerActivity.java
+++ app/src/main/java/com/example/parkingmanager/TwoWheelerActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.widget.GridView;
 
