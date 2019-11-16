fc01eefd72aa9855893b89026466d014dd8efe97
manasagoriparthi <manasaprasad97@gmail.com>
2019-11-15 21:49:42 -0600

    operating system builder @ manasa

--- app/src/main/java/com/example/parkingmanager/TwoWheelerActivity.java
+++ app/src/main/java/com/example/parkingmanager/TwoWheelerActivity.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.icu.text.SimpleDateFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.GridView;
 
