99581235f0087ef4d9766a54ca0c2ddbc148535a
manasagoriparthi <manasaprasad97@gmail.com>
2019-11-20 17:37:45 -0600

    created object for custom adapter to gridview @ manasa

--- app/src/main/java/com/example/parkingmanager/TwoWheelerActivity.java
+++ app/src/main/java/com/example/parkingmanager/TwoWheelerActivity.java
@@ -77,8 +77,8 @@ public class TwoWheelerActivity extends AppCompatActivity implements TwoWheelerA
             // prepared arraylist and passed it to the Adapter class
             mAdapter = new TwoWheelerAdapter(context, slotModels);
            mAdapter.setSlotClickListnerDownload(TwoWheelerActivity.this);
-            // Set custom adapter to gridview
-            //gridView = (GridView) findViewById(R.id.gridview);
+            //Set custom adapter to gridview
+            gridView = (GridView) findViewById(R.id.gridview);
            // gridView.setAdapter(mAdapter);
         }catch (Exception e){}
     }
