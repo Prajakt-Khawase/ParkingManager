4347a3897ad307b555774c3062609426b5d8b721
manasagoriparthi <manasaprasad97@gmail.com>
2019-11-20 18:45:24 -0600

    clear flags in SlotBookingActivity.java @ manasa

--- app/src/main/java/com/example/parkingmanager/SlotBookingActivity.java
+++ app/src/main/java/com/example/parkingmanager/SlotBookingActivity.java
@@ -33,7 +33,7 @@ public class SlotBookingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_slot_booking);
         context = SlotBookingActivity.this;
         Window window = getWindow();
-        //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
        // window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             //getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark)); //status bar or the time bar at the top
