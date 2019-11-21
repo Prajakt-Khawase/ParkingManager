8a9fe7e9d6c70476b2bed972320a588611eadbd4
manasagoriparthi <manasaprasad97@gmail.com>
2019-11-20 18:43:30 -0600

    window object created in SlotBookingActivity.java @ manasa

--- app/src/main/java/com/example/parkingmanager/SlotBookingActivity.java
+++ app/src/main/java/com/example/parkingmanager/SlotBookingActivity.java
@@ -32,7 +32,7 @@ public class SlotBookingActivity extends AppCompatActivity {
        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slot_booking);
         context = SlotBookingActivity.this;
-        //Window window = getWindow();
+        Window window = getWindow();
         //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
        // window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
