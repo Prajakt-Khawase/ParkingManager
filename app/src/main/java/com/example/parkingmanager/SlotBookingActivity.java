802a77c7e9fc0f9be0dd03b403ad902391b0934a
manasagoriparthi <manasaprasad97@gmail.com>
2019-11-20 18:37:34 -0600

    on create method created SlotBookingActivity.java @ manasa

--- app/src/main/java/com/example/parkingmanager/SlotBookingActivity.java
+++ app/src/main/java/com/example/parkingmanager/SlotBookingActivity.java
@@ -27,5 +27,45 @@ public class SlotBookingActivity extends AppCompatActivity {
     String owner, type, mobile, vehicleno, dateTime, manager,email;
     int slotno,slotid;
     SQLiteHelper mSQLiteHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_slot_booking);
+        //context = SlotBookingActivity.this;
+        //Window window = getWindow();
+        //window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+       // window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark)); //status bar or the time bar at the top
+       // }
+        //mSQLiteHelper = new SQLiteHelper(this);
+       // initialize();
 
-}
+        //back.setOnClickListener(new View.OnClickListener() {
+           // @Override
+           // public void onClick(View v) {
+               // finish();
+           // }
+       // });
+
+       // book.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+           // public void onClick(View v) {
+
+                //validation();
+            //}
+       // });
+
+      //  manager= SharedPrefrenceUtilities.getSPstringValue(context,SharedPrefrenceUtilities.spFirstName);
+
+
+    }
+
+    private void initialize() {
+
+    }
+
+    private void validation(){
+
+    }
+    }
