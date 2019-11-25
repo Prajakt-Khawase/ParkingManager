ada2d9252d589540bd2de9028658ea06f41185ff
mushafiq <43075639+mushafiq@users.noreply.github.com>
2019-11-24 22:52:21 -0600

    initialized paymentPagr

--- app/src/main/java/com/example/parkingmanager/PaymentPage.java
+++ app/src/main/java/com/example/parkingmanager/PaymentPage.java
@@ -1,23 +1,46 @@
 package com.example.parkingmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PaymentPage extends AppCompatActivity {
 
+    String owner,vehicleno,starttime,endtime,manager,totaltime,charge,slot,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment_page); 
-    }
-    public void doneAction(View v){
-        startActivity(new Intent(PaymentPage.this, CustomerLogout.class));
+        setContentView(R.layout.activity_payment_page);
+
+
+
+        initialize();
+        findViewById(R.id.payment_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
 
 
     }
 
+    private void initialize() {
+        TextView slotText = findViewById(R.id.payment_slot);
+        TextView ownerText = findViewById(R.id.payment_owner);
+        TextView vehiclenoText = findViewById(R.id.payment_vhicleno);
+
+
+    }
 
 }
